e55af5208abd135092f78935aa24a95e85ff913c
samiksha <samikshawankhade11@gmail.com>
2023-01-17 20:01:08 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://chatting-31tv.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
         body: JSON.stringify({
             prompt: data.get('prompt')
         })
-    })
+      })
 
     clearInterval(loadInterval)
     messageDiv.innerHTML = " "
